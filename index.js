d61fe7ac9b4acda057ea3bac30cd2d283f7fed10
Oleg Apostol <apostol@yandex-team.ru>
2014-10-24 02:27:37 +0400

    кнопка перспективит

--- index.js
+++ index.js
@@ -8,4 +8,40 @@ $(function() {
     //     out: fn()
     // })
 
+
+    $('button').on('click', function(e) {
+        var wrap = $('.answer').parent();
+
+        if(wrap.hasClass('notify')) {
+            $('body').removeClass('scene');
+            wrap.removeClass('notify');
+        } else {
+            $('body').addClass('scene');
+            wrap
+                .addClass('notify')
+                .one('webkitTransitionEnd', function() {
+                    var times = 0,
+                        duration = 1,
+                        timer;
+
+                    timer = setInterval(function() {
+                        setTransition(wrap, duration+=times * .13)
+
+                        console.log(times);
+                        if(++times === 5) {
+                            clearTimeout(timer);
+                            setTransition(wrap, 2);
+                        }
+                    }, 500);
+
+                });
+
+        }
+
+    });
+
+    setTransition = function(elem, val) {
+        elem.css('-webkit-transition-duration', val + 's');
+    }
+
 });
