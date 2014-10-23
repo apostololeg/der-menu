7f96ec53ef312aeea451cf3daf5b4b4c3107d3e2
Oleg Apostol <apostol@yandex-team.ru>
2014-10-23 22:31:37 +0400

    style

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ $(function() {
     // var board = new Board(4);
     var controller = new tabController($('.control_active'));
 
-    controller.domElem.on({
-        enter: fn(),
-        out: fn()
-    })
+    // controller.domElem.on({
+    //     enter: fn(),
+    //     out: fn()
+    // })
 
 });
